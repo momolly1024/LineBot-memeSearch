82620a76cb21032a88da21068218cb824d3f591c
MollyCognito <momolly1024@gmail.com>
2023-02-07 10:05:12 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,45 @@
+# LineBot for Meme Search
+
+It's a multiple language support linebot for meme search.
+
+### LineID：@740tlgkt
+
+-   Enter a keyword to search and will receive 3 related images.(JPG)
+
+-   add「mp4」in keyword，will receive 3 related video.(mp4)
+-   Enter the word「popular」，will receive 3 latest popular images.
+-   Enter the word「random」，will receive a random image.
+
+![](https://i.imgur.com/soggf8W.png)
+
+# LineBot 迷因圖片搜尋
+
+支援多語系的迷因搜尋 Linebot.
+
+### Line 帳號：@740tlgkt
+
+-   輸入想搜尋的關鍵字，會收到 3 張相關的圖片(.JPG)
+
+-   加上「動態」，會收到影片檔(.mp4)
+-   輸入「熱門」，會收到 3 張近期熱門迷因圖片
+-   輸入「隨機」，會收到一張隨機圖片
+
+#### -
+
+![](https://i.imgur.com/v040EOS.png)
+![](https://i.imgur.com/3hWfEqI.png)
+![](https://i.imgur.com/QJAMls2.png)
+
+✨ 免責聲明
+所有圖檔皆由 API 回傳，無法保證是否正確/合法，大家使用上要小心注意，有任何疑慮請停止使用。
+
+### 開發過程
+
+-   Fast API
+-   py -m venv venv
+-   .\venv\Script\activate
+-   .gitignore
+-   main.py
+-   requirements.txt
+-   pip install -r requirements.txt
+-   uvicorn main:app --reload
---
+++ main.py
@@ -0,0 +1,146 @@
+from fastapi import FastAPI, Request, HTTPException
+
+from linebot import (
+    LineBotApi, WebhookHandler
+)
+from linebot.exceptions import (
+    InvalidSignatureError,LineBotApiError
+)
+from linebot.models import (
+    MessageEvent, TextMessage, TextSendMessage,VideoSendMessage,ImageSendMessage
+)
+import json
+import requests
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+line_bot_api = LineBotApi(os.getenv("Channel_access_token"))
+handler = WebhookHandler(os.getenv("Channel_secret"))
+api_key = os.getenv("GiphyAPI_Key")
+
+app = FastAPI()
+ 
+@app.post("/")
+async def echoBot(request: Request):
+    signature = request.headers["X-Line-Signature"]
+    body = await request.body()
+    try:
+        handler.handle(body.decode(), signature)
+    except InvalidSignatureError:
+        raise HTTPException(status_code=400, detail="Missing Parameters")
+    return "OK"
+
+@handler.add(MessageEvent, message=(TextMessage))
+def handling_message(event):
+    replyToken = event.reply_token
+    
+    if isinstance(event.message, TextMessage):
+        if "動態" in event.message.text or 'mp4'in event.message.text: 
+            if event.message.text=='隨機' or event.message.text=='random':
+                line_bot_api.reply_message(event.reply_token, VideoSendMessage(original_content_url=getRandom(), preview_image_url=getRandom()))
+            elif event.message.text=='熱門' or event.message.text=='popular':
+                data = []
+                for i in getTrend():
+                    data.append(VideoSendMessage(
+                        original_content_url=i,
+                        preview_image_url=i,
+                    ))
+                line_bot_api.reply_message(event.reply_token, data)
+            else:
+                try:
+                    data = []
+                    for i in getSearch(event.message.text):
+                        data.append(VideoSendMessage(
+                        original_content_url=i,
+                        preview_image_url=i,
+                    ))
+                    line_bot_api.reply_message(event.reply_token, data)            
+                except LineBotApiError as e:
+                    errorText=f"找不到與 {event.message.text} 有關的迷因圖片"
+                    line_bot_api.reply_message(event.reply_token,TextSendMessage(text=errorText))
+
+        else:
+            if event.message.text=='隨機' or event.message.text=='random':            
+                line_bot_api.reply_message(event.reply_token, ImageSendMessage(
+                original_content_url=getRandom(),
+                preview_image_url=getRandom()))
+            elif event.message.text=='熱門' or event.message.text=='popular':
+                data = []
+                for i in getTrend():
+                    data.append(ImageSendMessage(
+                        original_content_url=i,
+                        preview_image_url=i,
+                    ))
+                line_bot_api.reply_message(event.reply_token, data)
+            else:
+                try:
+                    data = []
+                    for i in getSearch(event.message.text):
+                            data.append(ImageSendMessage(
+                                original_content_url=i,
+                                preview_image_url=i,
+                    ))
+                    line_bot_api.reply_message(event.reply_token, data)            
+                except LineBotApiError as e:
+                    errorText=f"找不到與 {event.message.text} 有關的迷因圖片"
+                    line_bot_api.reply_message(event.reply_token,TextSendMessage(text=errorText))
+
+                
+            messages = event.message.text
+            
+            echoMessages = TextSendMessage(text=messages)
+            line_bot_api.reply_message(reply_token=replyToken, messages=echoMessages)
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+def getSearch(keyWord):
+    
+    response = requests.get(f"https://api.giphy.com/v1/gifs/search?api_key={api_key}&q={keyWord}&limit=3")
+    res=[]
+    
+    if response.status_code == 200:
+        data = response.json()
+        gifs = data["data"]
+        for i, gif in enumerate(gifs):
+            gif_url = gif["images"]["original"]["url"]
+            res.append(gif_url)
+        return res     
+    else:
+        print("Search request failed with status code ", response.status_code)
+
+
+
+# 熱門前10張
+def getTrend():
+    url = f"https://api.giphy.com/v1/gifs/trending?api_key={api_key}&limit=3"
+    response = requests.get(url)
+    res=[]  
+    if response.status_code == 200:
+        data = json.loads(response.content.decode("utf-8"))
+        gifs = data["data"]
+        for i, gif in enumerate(gifs):
+            gif_url = gif["images"]["original"]["url"]
+            res.append(gif_url)
+        return res  
+    else:
+        print("Something went wrong")
+
+
+# 隨機一張
+def getRandom():
+    response = requests.get(f"http://api.giphy.com/v1/gifs/random?api_key={api_key}&tag=meme")
+    if response.status_code == 200:
+        gifs = response.json()['data']
+        return gifs["images"]["original"]["url"]
+
+
